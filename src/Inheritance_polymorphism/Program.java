5906571cc0782f840511477f52479a50b51e7f74
Gleides Vinente <gleidevelop@gmail.com>
2020-09-13 12:44:01 -0400

    Herança e Polimorfismo

--- src/Inheritance_polymorphism/Program.java
+++ src/Inheritance_polymorphism/Program.java
@@ -4,16 +4,17 @@ import Inheritance_polymorphism.entities.Account;
 import Inheritance_polymorphism.entities.BusinessAccount;
 import Inheritance_polymorphism.entities.SavingsAccount;
 
-public class ProgramInheritance {
+public class Program {
     public static void main(String[] args) {
-        progrSobreCarga();
-//        progrHeranca();
+        progrPolimorfismo();
+        /*progHeranca();
+        progSobreCarga();*/
     }
 
     /**
      * Sobrecarga ou Sobreposição: É a implementação do método da superclasse no método da subclasse.
      */
-    public static void progrSobreCarga(){
+    public static void progSobreCarga(){
         Account account = new Account(1001, "Alex", 1000.0);
         account.withdraw(200.0);
         System.out.println(account.getBalance());
@@ -31,7 +32,7 @@ public class ProgramInheritance {
      * Tem uma Relação "é-um"
      * Generalização/Especialização
      */
-    public static void progrHeranca() {
+    public static void progHeranca() {
         Account account = new Account(1011, "Alex",0.0);
         BusinessAccount businessAccount = new BusinessAccount(1002, "Maria",0.0, 500.0);
 
@@ -59,5 +60,20 @@ public class ProgramInheritance {
             System.out.println("Update!");
         }
     }
+    /**
+     * Herança: É um recurso que permite que variáveis do mesmo tipo mais genérico possam apontar para
+     * objetos de tipos específicos diferentes, tendo assim comportamentos diferentes conforme cada tipo especifíco.
+     * UPCASTING: Associação do tipo específico com o tipo genérico é feita em tempo de execução.
+     * Compilador: Não sabe para qual tipo ESPECÍFICO (Instância) a chamada do método Withdraw está sendo feita,
+     * ele só sabe que são duas variáveis do tipo GENÉRICO (Account).
+     */
 
+    public static void progrPolimorfismo() {
+        Account x = new Account(1020, "Alex",1000.0);
+        Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+        x.withdraw(50.0);
+        y.withdraw(50.0);
+        System.out.println("Account: "+ x.getBalance());
+        System.out.println("SavingsAccount: "+ y.getBalance());
+    }
 }
