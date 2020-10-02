fe734c91a38e537805c6b6139769b4063013de34
Gleides Vinente <gleidevelop@gmail.com>
2020-10-01 21:13:24 -0400

    Bad solution

--- exceptions-java/src/application/ProgramException.java
+++ exceptions-java/src/application/ProgramException.java
@@ -5,10 +5,12 @@ import model.entities.Reservation;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ProgramException {
     public static void main(String[] args) throws ParseException {
+        Locale.setDefault(Locale.US);
         Scanner sc =new Scanner(System.in);
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
@@ -31,15 +33,13 @@ public class ProgramException {
             System.out.print("Check-out date (dd/MM/yyyy): ");
             checkOut = sdf.parse(sc.next());
 
-            Date now = new Date();
-            if(checkIn.before(now)||checkOut.before(now)){
-                System.out.println("Error in reservation: Reservation dates for update must be future");
-            }else if(!checkOut.after(checkIn)){
-                System.out.println("Error in reservation: Check-out date must be after Check-in date");
-            }else {
-                reservation.updateDates(checkIn,checkOut);
-                System.out.println("Reservation: " + reservation + "\n");
+            String error =  reservation.updateDates(checkIn,checkOut);
+            if(error != null){
+                System.out.println("Error in Reservation: " + reservation + "\n");
+            }else{
+                System.out.println("Reservation: " + reservation);
             }
+
         }
         sc.close();
     }
