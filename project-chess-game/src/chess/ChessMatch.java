a7150c7583d6e45b502b209e6e11f8842aee19a8
Gleides Vinente <gleidevelop@gmail.com>
2020-10-06 00:02:01 -0400

    Moving pieces

--- project-chess-game/src/chess/ChessMatch.java
+++ project-chess-game/src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -20,6 +22,28 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+
+    public ChessPiece performChessMove(ChessPosition source, ChessPosition target){
+        Position mSource =  source.toPosition();
+        Position mTarget = target.toPosition();
+        validateSourcePosition(mSource);
+        Piece capturePiece = makeMove(mSource,mTarget);
+        return (ChessPiece) capturePiece;
+    }
+
+    private Piece makeMove(Position mSource, Position mTarget) {
+        Piece mPiece = board.removePiece(mSource);
+        Piece capturePiece = board.removePiece(mTarget);
+        board.placePiece(mPiece, mTarget);
+        return capturePiece;
+    }
+
+    private void validateSourcePosition(Position mSource) {
+        if(board.thereIsAPiece(mSource)){
+            throw new ChessException("There is no piece on source position");
+        }
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece){
