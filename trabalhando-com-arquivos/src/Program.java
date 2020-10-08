ee3db1cdaa693594966e334370f43fac6906796a
Gleides Vinente <gleidevelop@gmail.com>
2020-10-07 23:07:19 -0400

    creating and read file

--- trabalhando-com-arquivos/src/Program.java
+++ trabalhando-com-arquivos/src/Program.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Program {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        File file = new File("trabalhando-com-arquivos/in.txt");
+        Scanner sc = null;
+        try {
+            sc =  new Scanner(file);
+            while (sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }finally {
+            if(sc != null){
+                sc.close();
+            }
+        }
+    }
 }
