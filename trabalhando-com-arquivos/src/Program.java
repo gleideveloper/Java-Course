4b92204c3ba16a3048b4af205359b443f542d0a7
Gleides Vinente <gleidevelop@gmail.com>
2020-10-07 23:26:00 -0400

    Bloco try-witch-resources

--- trabalhando-com-arquivos/src/Program.java
+++ trabalhando-com-arquivos/src/Program.java
@@ -1,17 +1,12 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         String path = "trabalhando-com-arquivos/in.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
             String line = br.readLine();
 
@@ -21,17 +16,6 @@ public class Program {
             }
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
