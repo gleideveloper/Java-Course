9197595cc3758743b71a1c18c613b680129ace68
Gleides Vinente <gleidevelop@gmail.com>
2020-10-08 21:27:22 -0400

    write file with buffer

--- trabalhando-com-arquivos/src/Program.java
+++ trabalhando-com-arquivos/src/Program.java
@@ -1,9 +1,12 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Program {
     public static void main(String[] args) {
+//        usingBufferedReaderFileReader();
+        usingBufferedWriterFileWriter();
+    }
+
+    private static void usingBufferedReaderFileReader() {
         String path = "trabalhando-com-arquivos/in.txt";
 
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
@@ -13,6 +16,20 @@ public class Program {
             while (line != null) {
                 System.out.println(line);
                 line = br.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void usingBufferedWriterFileWriter(){
+        String[] lines = new String[]{"Good Norming","Good Afternoon","Good night"};
+        String path = "trabalhando-com-arquivos/out.txt";
+        //append = true -> indica que o arquivo nao vai ser recriado
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path,true))) {
+            for (String line: lines) {
+                bw.write(line);
+                bw.newLine();
             }
         } catch (IOException e) {
             e.printStackTrace();
