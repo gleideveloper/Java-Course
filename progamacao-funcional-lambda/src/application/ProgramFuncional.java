6454f0d84a27d358beadd92491e68a7bf514e3fc
Gleides Vinente <gleidevelop@gmail.com>
2020-11-26 21:50:38 -0400

    arrow function expressao lambda

--- progamacao-funcional-lambda/src/application/ProgramFuncional.java
+++ progamacao-funcional-lambda/src/application/ProgramFuncional.java
@@ -3,10 +3,11 @@ package application;
 import entities.Product;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ProgramFuncional {
+
     public static void main(String[] args) {
         List<Product> list = new ArrayList<>();
 
@@ -17,7 +18,26 @@ public class ProgramFuncional {
         list.add(new Product("Banana", 10.0));
         list.add(new Product("Tablet", 450.0));
 
-        list.sort(new MyComparator());
+        /* 1º forma passando um comparator
+        list.sort(new MyComparator()); */
+
+        /* 2º forma passando uma class anômima
+        Comparator<Product> comp = new Comparator<Product>() {
+            @Override
+            public int compare(Product p1, Product p2) {
+                return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+            }
+        };*/
+
+        /* 3º forma passando uma expressao Lambda[Arow Function] e definindo um função anômima
+        Comparator<Product> comp = (p1, p2) -> {
+                return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+        };*/
+
+        /* 4º forma passando uma expressao Lambda[Arrow Function] e definindo um função anômima */
+        Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+
+        list.sort(comp);
 
         for (Product p : list) {
             System.out.println(p);
